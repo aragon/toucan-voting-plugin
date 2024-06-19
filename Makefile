9aa9fc8806809ce50f3c65235af53cc397764899
Jordaniza <jordan.imran@predixa.ai>
2024-06-19 15:50:37 +0400

    f: deployment scripts

--- Makefile
+++ Makefile
@@ -1,9 +1,41 @@
-allow-scripts: 
-	chmod +x script/bash/*.sh
+# include .env file and export its env vars
+# (-include to ignore error if it does not exist)
+-include .env
 
-coverage:
-	./script/bash/coverage.sh
+# env var check
+check-env :; echo $(ETHERSCAN_API_KEY)
+
+allow-scripts:; chmod +x script/bash/*.sh
+
+coverage:; ./script/bash/coverage.sh
 
 # create an HTML coverage report in ./report (requires lcov & genhtml)
-coverage-report:
-	./script/bash/coverage-report.sh
+coverage-report:; ./script/bash/coverage-report.sh
+
+deploy-sepolia:; export EXECUTION_OR_VOTING=EXECUTION && forge script DeployToucan \
+	--rpc-url https://sepolia.infura.io/v3/$(API_KEY_INFURA) \
+	--private-key $(PRIVATE_KEY) \
+	--broadcast \
+	--verify \
+	--etherscan-api-key $(ETHERSCAN_API_KEY) \
+	-vvvvv
+
+deploy-arbitrum-sepolia:; export EXECUTION_OR_VOTING=VOTING && forge script DeployToucan \
+	--rpc-url https://arbitrum-sepolia.infura.io/v3/$(API_KEY_INFURA) \
+	--private-key $(PRIVATE_KEY) \
+	--broadcast \
+	--verify \
+	--etherscan-api-key $(ARBISCAN_API_KEY) \
+	-vvvvv
+
+script-sepolia :; export EXECUTION_OR_VOTING=EXECUTION && forge script ExecuteDemoOffsite \
+	--rpc-url https://sepolia.infura.io/v3/$(API_KEY_INFURA) \
+	--private-key $(PRIVATE_KEY) \
+	--broadcast \
+	-vvv
+
+script-arbitrum-sepolia :; export EXECUTION_OR_VOTING=VOTING && forge script ExecuteDemoOffsite \
+	--rpc-url https://arbitrum-sepolia.infura.io/v3/$(API_KEY_INFURA) \
+	--private-key $(PRIVATE_KEY) \
+	--broadcast \
+	-vvv
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,103 +1,325 @@
 // // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.20;
+pragma solidity ^0.8.20;
 
-// import {Script, console2} from "forge-std/Script.sol";
-// import {Vm} from "forge-std/Vm.sol";
+import {Script, console2} from "forge-std/Script.sol";
+import {Vm} from "forge-std/Vm.sol";
 
-// import {DAOFactory} from "@aragon/osx/framework/dao/DAOFactory.sol";
-// import {PluginRepoFactory} from "@aragon/osx/framework/plugin/repo/PluginRepoFactory.sol";
-// import {PluginRepo} from "@aragon/osx/framework/plugin/repo/PluginRepo.sol";
-// import {hashHelpers, PluginSetupRef} from "@aragon/osx/framework/plugin/setup/PluginSetupProcessorHelpers.sol";
+// external contracts
+import {OApp} from "@lz-oapp/OApp.sol";
+import {OFT} from "@lz-oft/OFT.sol";
+import {Origin} from "@lz-oapp/interfaces/IOAppReceiver.sol";
+import {SendParam, OFTLimit, OFTFeeDetail, OFTReceipt, MessagingFee} from "@lz-oft/interfaces/IOFT.sol";
+import {OptionsBuilder} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/libs/OptionsBuilder.sol";
+import {IVotesUpgradeable} from "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.sol";
 
-// import {MyPlugin} from "../src/MyPlugin.sol";
-// import {MyPluginSetup} from "../src/MyPluginSetup.sol";
+// aragon contracts
+import {IDAO} from "@aragon/osx-commons-contracts/src/dao/IDAO.sol";
+import {DAO} from "@aragon/osx/core/dao/DAO.sol";
+import {ProxyLib} from "@aragon/osx-commons-contracts/src/utils/deployment/ProxyLib.sol";
+import {GovernanceERC20} from "@aragon/token-voting/ERC20/governance/GovernanceERC20.sol";
+import {TokenVoting as ToucanVoting} from "@aragon/token-voting/TokenVoting.sol";
+import {ITokenVoting, IVoteContainer} from "@aragon/token-voting/ITokenVoting.sol";
 
-// contract MyPluginScript is Script {
-//     address pluginRepoFactory;
-//     DAOFactory daoFactory;
-//     string nameWithEntropy;
-//     address[] pluginAddress;
+import {DAO} from "@aragon/osx/core/dao/DAO.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+// external test utils
+import "forge-std/console2.sol";
+import {TestHelper} from "@lz-oapp-test/TestHelper.sol";
 
-//     function setUp() public {
-//         pluginRepoFactory = vm.envAddress("PLUGIN_REPO_FACTORY");
-//         daoFactory = DAOFactory(vm.envAddress("DAO_FACTORY"));
-//         nameWithEntropy = string.concat("my-plugin-", vm.toString(block.timestamp));
-//     }
+// internal contracts
+import {GovernanceOFTAdapter} from "@execution-chain/crosschain/GovernanceOFTAdapter.sol";
+import {GovernanceERC20VotingChain} from "@voting-chain/token/GovernanceERC20VotingChain.sol";
+import {OFTTokenBridge} from "@voting-chain/crosschain/OFTTokenBridge.sol";
 
-//     function run() public {
-//         // 0. Setting up Foundry
-//         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+import {ToucanRelay} from "@voting-chain/crosschain/ToucanRelay.sol";
+import {ToucanReceiver} from "@execution-chain/crosschain/ToucanReceiver.sol";
+import {ProposalIdCodec} from "@libs/ProposalIdCodec.sol";
 
-//         // 1. Deploying the Plugin Setup
-//         MyPluginSetup pluginSetup = deployPluginSetup();
+// internal test utils
+import "@utils/converters.sol";
+import "@utils/deployers.sol";
+import "forge-std/console2.sol";
 
-//         // 2. Publishing it in the Aragon OSx Protocol
-//         PluginRepo pluginRepo = deployPluginRepo(address(pluginSetup));
+import {MockDAOSimplePermission as MockDAO} from "test/mocks/MockDAO.sol";
+import {AragonTest} from "test/base/AragonTest.sol";
 
-//         // 3. Defining the DAO Settings
-//         DAOFactory.DAOSettings memory daoSettings = getDAOSettings();
+contract DeployToucan is Script {
+    struct LzChain {
+        uint32 eid;
+        address endpoint;
+    }
 
-//         // 4. Defining the plugin settings
-//         DAOFactory.PluginSettings[] memory pluginSettings = getPluginSettings(pluginRepo);
+    address deployer;
+    address constant JUAR = 0x8bF1e340055c7dE62F11229A149d3A1918de3d74;
+    address constant ME = 0x7771c1510509C0dA515BDD12a57dbDd8C58E5363;
 
-//         // 5. Deploying the DAO
-//         vm.recordLogs();
-//         address createdDAO = address(daoFactory.createDao(daoSettings, pluginSettings));
+    LzChain SEPOLIA = LzChain(40161, 0x6EDCE65403992e310A62460808c4b910D972f10f);
+    LzChain ARBITRUM_SEPOLIA = LzChain(40231, 0x6EDCE65403992e310A62460808c4b910D972f10f);
 
-//         // 6. Getting the Plugin Address
-//         Vm.Log[] memory logEntries = vm.getRecordedLogs();
+    using OptionsBuilder for bytes;
+    using ProxyLib for address;
+    using ProposalIdCodec for uint256;
 
-//         for (uint256 i = 0; i < logEntries.length; i++) {
-//             if (
-//                 logEntries[i].topics[0] ==
-//                 keccak256("InstallationApplied(address,address,bytes32,bytes32)")
-//             ) {
-//                 pluginAddress.push(address(uint160(uint256(logEntries[i].topics[2]))));
-//             }
-//         }
+    // execution chain
+    uint32 EID_EXECUTION_CHAIN = SEPOLIA.eid;
+    // uint256  EVM_EXECUTION_CHAIN = 137;
 
-//         vm.stopBroadcast();
+    address daoExecutionChain;
+    GovernanceERC20 tokenExecutionChain;
+    GovernanceOFTAdapter adapter;
+    OFTTokenBridge bridge;
+    ToucanReceiver receiver;
+    address layerZeroEndpointExecutionChain = SEPOLIA.endpoint;
 
-//         // 7. Logging the resulting addresses
-//         console2.log("Plugin Setup: ", address(pluginSetup));
-//         console2.log("Plugin Repo: ", address(pluginRepo));
-//         console2.log("Created DAO: ", address(createdDAO));
-//         console2.log("Installed Plugins: ");
-//         for (uint256 i = 0; i < pluginAddress.length; i++) {
-//             console2.log("- ", pluginAddress[i]);
-//         }
-//     }
+    // voting chain
+    uint32 EID_VOTING_CHAIN = ARBITRUM_SEPOLIA.eid;
 
-//     function deployPluginSetup() public returns (MyPluginSetup) {
-//         MyPluginSetup pluginSetup = new MyPluginSetup();
-//         return pluginSetup;
-//     }
+    address daoVotingChain;
+    GovernanceERC20VotingChain tokenVotingChain;
+    ToucanRelay relay;
+    ToucanVoting plugin;
+    address layerZeroEndpointVotingChain = ARBITRUM_SEPOLIA.endpoint;
 
-//     function deployPluginRepo(address pluginSetup) public returns (PluginRepo pluginRepo) {
-//         pluginRepo = PluginRepoFactory(pluginRepoFactory).createPluginRepoWithFirstVersion(
-//             nameWithEntropy,
-//             pluginSetup,
-//             msg.sender,
-//             "0x00", // TODO: Give these actual values on prod
-//             "0x00"
-//         );
-//     }
+    // agents
+    address executionVoter = ME;
+    address votingVoter = JUAR;
+    // cash they get in governance tokens
+    uint constant INITIAL_MINT = 10_000 ether;
 
-//     function getDAOSettings() public view returns (DAOFactory.DAOSettings memory) {
-//         return DAOFactory.DAOSettings(address(0), "", nameWithEntropy, "");
-//     }
+    // voting params
+    uint32 constant SUPPORT_THRESHOLD = 1;
+    uint32 constant MIN_PARTICIPATION = 1;
+    uint64 constant MIN_DURATION = 3600;
+    uint constant MIN_PROPOSER_VOTING_POWER = 1;
+    ToucanVoting.VotingMode constant MODE = ITokenVoting.VotingMode.VoteReplacement;
 
-//     function getPluginSettings(
-//         PluginRepo pluginRepo
-//     ) public pure returns (DAOFactory.PluginSettings[] memory pluginSettings) {
-//         uint256 pluginCounterNumber = 1;
-//         bytes memory pluginSettingsData = abi.encode(pluginCounterNumber);
+    modifier broadcast() {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        deployer = vm.addr(deployerPrivateKey);
 
-//         PluginRepo.Tag memory tag = PluginRepo.Tag(1, 1);
-//         pluginSettings = new DAOFactory.PluginSettings[](1);
-//         pluginSettings[0] = DAOFactory.PluginSettings(
-//             PluginSetupRef(tag, pluginRepo),
-//             pluginSettingsData
-//         );
-//     }
-// }
+        vm.startBroadcast(deployer);
+        _;
+        vm.stopBroadcast();
+    }
+
+    function _executionChain() internal view returns (bool) {
+        string memory result = vm.envString("EXECUTION_OR_VOTING");
+        if (keccak256(abi.encodePacked(result)) == keccak256(abi.encodePacked("EXECUTION"))) {
+            return true;
+        } else if (keccak256(abi.encodePacked(result)) == keccak256(abi.encodePacked("VOTING"))) {
+            return false;
+        } else {
+            revert(
+                "Invalid env variable EXECUTION_OR_VOTING, must be either 'EXECUTION' or 'VOTING'"
+            );
+        }
+    }
+
+    function run() public broadcast {
+        if (_executionChain()) {
+            console2.log("Running Execution Chain");
+            runExecutionChain();
+            _connectOApps_executionChain();
+        } else {
+            console2.log("Running Voting Chain");
+            runVotingChain();
+            _connectOApps_votingChain();
+        }
+    }
+
+    function runVotingChain() public {
+        _deployVotingChain();
+
+        console2.log("Contracts deployed:");
+
+        console2.log("Voting Chain:");
+        console2.log("Bridge: ", address(bridge));
+        console2.log("Relay: ", address(relay));
+        console2.log("DAO: ", daoVotingChain);
+        console2.log("Token: ", address(tokenVotingChain));
+    }
+
+    function runExecutionChain() public {
+        _deployExecutionChain();
+
+        console2.log("Contracts deployed:");
+
+        console2.log("Execution Chain:");
+        console2.log("DAO: ", daoExecutionChain);
+        console2.log("Token: ", address(tokenExecutionChain));
+        console2.log("Adapter: ", address(adapter));
+        console2.log("Receiver: ", address(receiver));
+        console2.log("Plugin: ", address(plugin));
+    }
+
+    function _deployPlugin(
+        ToucanVoting.VotingSettings memory settings
+    ) internal returns (ToucanVoting) {
+        // deploy implementation
+        address base = address(new ToucanVoting());
+        // encode the initalizer
+        bytes memory data = abi.encodeCall(
+            ToucanVoting.initialize,
+            (IDAO(daoExecutionChain), settings, IVotesUpgradeable(tokenExecutionChain))
+        );
+        // deploy and return the proxy
+        address deployed = base.deployUUPSProxy(data);
+
+        return ToucanVoting(deployed);
+    }
+
+    function _mintSettings(
+        uint _qty
+    ) internal view returns (GovernanceERC20.MintSettings memory settings) {
+        address[] memory receivers = new address[](2);
+        uint256[] memory amounts = new uint256[](2);
+
+        receivers[0] = executionVoter;
+        amounts[0] = _qty;
+
+        receivers[1] = votingVoter;
+        amounts[1] = _qty;
+
+        settings = GovernanceERC20.MintSettings(receivers, amounts);
+        return settings;
+    }
+
+    function createMockDAO() public returns (DAO) {
+        return _createMockDAO(address(this));
+    }
+
+    function _deployExecutionChain() internal {
+        // setup the dao
+        daoExecutionChain = address(_createMockDAO(deployer));
+
+        // setup the token
+        tokenExecutionChain = new GovernanceERC20(
+            IDAO(daoExecutionChain),
+            "Execution Token",
+            "EXT",
+            _mintSettings(INITIAL_MINT)
+        );
+
+        // initalize the adapter, receiver and plugin
+        ToucanVoting.VotingSettings memory settings = ITokenVoting.VotingSettings({
+            votingMode: MODE,
+            supportThreshold: SUPPORT_THRESHOLD,
+            minParticipation: MIN_PARTICIPATION,
+            minDuration: MIN_DURATION,
+            minProposerVotingPower: MIN_PROPOSER_VOTING_POWER
+        });
+
+        plugin = _deployPlugin(settings);
+
+        receiver = deployToucanReceiver({
+            _governanceToken: address(tokenExecutionChain),
+            _lzEndpoint: layerZeroEndpointExecutionChain,
+            _dao: daoExecutionChain,
+            _votingPlugin: address(plugin)
+        });
+
+        adapter = new GovernanceOFTAdapter({
+            _token: address(tokenExecutionChain),
+            _voteProxy: address(receiver),
+            _lzEndpoint: layerZeroEndpointExecutionChain,
+            _dao: daoExecutionChain
+        });
+
+        // authorize the receiver to send to the plugin
+        DAO(payable(daoExecutionChain)).grant({
+            _where: address(receiver),
+            _who: address(this),
+            _permissionId: receiver.RECEIVER_ADMIN_ID()
+        });
+
+        // configure the OApps
+        adapter.setPeer(EID_VOTING_CHAIN, addressToBytes32(address(bridge)));
+        receiver.setPeer(EID_VOTING_CHAIN, addressToBytes32(address(relay)));
+    }
+
+    function _deployVotingChain() internal {
+        daoVotingChain = address(_createMockDAO(deployer));
+
+        // setup the token
+        tokenVotingChain = new GovernanceERC20VotingChain({
+            _dao: IDAO(daoVotingChain),
+            _name: "Voting Token",
+            _symbol: "VOT"
+        });
+
+        // initalize the bridge and relay
+        relay = deployToucanRelay(
+            address(tokenVotingChain),
+            layerZeroEndpointVotingChain,
+            daoVotingChain
+        );
+
+        bridge = new OFTTokenBridge({
+            _token: address(tokenVotingChain),
+            _lzEndpoint: layerZeroEndpointVotingChain,
+            _dao: daoVotingChain
+        });
+
+        // we need to allow the bridge to mint and burn tokens
+        // todo - these should be done in the setup contracts.
+        DAO(payable(daoVotingChain)).grant({
+            _where: address(tokenVotingChain),
+            _who: address(bridge),
+            _permissionId: tokenVotingChain.MINT_PERMISSION_ID()
+        });
+
+        DAO(payable(daoVotingChain)).grant({
+            _where: address(tokenVotingChain),
+            _who: address(bridge),
+            _permissionId: tokenVotingChain.BURN_PERMISSION_ID()
+        });
+
+        // configure the OApps
+        bridge.setPeer(EID_EXECUTION_CHAIN, addressToBytes32(address(adapter)));
+        relay.setPeer(EID_EXECUTION_CHAIN, addressToBytes32(address(receiver)));
+    }
+
+    function _createMockDAO(address _initalOwner) internal returns (DAO) {
+        DAO _dao = DAO(payable(new ERC1967Proxy(address(new DAO()), bytes(""))));
+        string memory _daoURI = "ipfs://";
+        _dao.initialize({
+            _metadata: bytes(""),
+            _initialOwner: _initalOwner,
+            _trustedForwarder: address(0),
+            daoURI_: _daoURI
+        });
+
+        _dao.grant({
+            _who: address(_dao),
+            _where: address(_dao),
+            _permissionId: _dao.ROOT_PERMISSION_ID()
+        });
+
+        return _dao;
+    }
+
+    function _connectOApps_executionChain() internal {
+        // the oftadapter and the bridge have a bidrectional relationship
+        adapter.setPeer(EID_VOTING_CHAIN, addressToBytes32(address(bridge)));
+        receiver.setPeer(EID_VOTING_CHAIN, addressToBytes32(address(relay)));
+    }
+
+    function _connectOApps_votingChain() internal {
+        bridge.setPeer(EID_EXECUTION_CHAIN, addressToBytes32(address(adapter)));
+        relay.setPeer(EID_EXECUTION_CHAIN, addressToBytes32(address(receiver)));
+    }
+
+    function _connectOApps() internal {
+        if (_executionChain()) {
+            _connectOApps_executionChain();
+        } else {
+            _connectOApps_votingChain();
+        }
+    }
+
+    function getProposalTally(
+        uint256 _proposalId
+    ) internal view returns (IVoteContainer.Tally memory tally) {
+        (, , , tally, , ) = plugin.getProposal(_proposalId);
+    }
+}
